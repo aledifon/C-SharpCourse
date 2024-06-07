828de1ce71bbeffbdd0f66d3c247f9f9c7c33ac2
alejandro.diazfontal <alejandro.diazfontalva@less-sa.com>
2024-06-07 16:03:44 +0200

    minor modifs

---
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// takes the user input and stores it
+string userInput = Console.ReadLine();
+
+Console.WriteLine("You entered " + userInput);
+Console.ReadKey();
