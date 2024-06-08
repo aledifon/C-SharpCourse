88228d6d21757138e2146cd8e256f6dfc7934b39
aledifon <aledifon89@gmail.com>
2024-06-08 14:50:29 +0200

    Variable Definition Test on Hello World

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,6 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World weyy!");
 
+// Reference Type
+string myName = "Denis";
+
+// defining or setting up a variable
+string petsName;
+
+// initialize variable
+petsName = "Daisy";
+Console.WriteLine($"my pet is {petsName}");
+
+// assign a new value
+petsName = "Barky";
+Console.WriteLine($"my pet is {petsName}");
+
+Console.ReadKey();
+
+// Value Types
+int myAge = 35;
+//double pi = -3.14;
+
+byte age = 255;
+
+short linkdinConnections = 32550;
+
+long phoneNumber = 01765453434;
 
 // declare a variable
 string myFriendsName;
