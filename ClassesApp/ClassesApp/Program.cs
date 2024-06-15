6dd8ad0fd075bd67a12b9f7d1f1ef968954d9810
aledifon <aledifon89@gmail.com>
2024-06-15 19:57:12 +0200

    Finished const & read-only properties more examples + Started the Quiz Project

--- ClassesApp/ClassesApp/Program.cs
+++ ClassesApp/ClassesApp/Program.cs
@@ -5,30 +5,11 @@
     {
         static void Main(string[] args)
         {
-            //Car car = new Car();
-            //Car car2 = new Car();
-            //Car car3 = new Car("A3","Audi",false);
-            //car3.Drive();
+            Rectangle rectangle1 = new Rectangle("Red");
+            Rectangle rectangle2 = new Rectangle("Blue");
 
-            //// accesing the public static variable NumberOfCars of the Car Class
-            //Console.WriteLine("Number of cars produced: " + Car.NumberOfCars);            
-
-            // Reading a private read-only field
-
-            Customer customer = new Customer();
-            Customer customer2 = new Customer("John Doe");
-            Customer customer3 = new Customer();
-
-            Console.WriteLine("The 1st Customer ID is " + customer.Id + "\n");
-            Console.WriteLine("The 2nd Customer ID is " + customer2.Id + "\n");
-            Console.WriteLine("The 3rd Customer ID is " + customer3.Id + "\n");
-
-            customer.GetDetails();
-            customer2.GetDetails();
-            customer3.Password = "12431241234fasdf";
-            customer3.GetDetails();
-
-                       
+            rectangle1.DisplayDetails();
+            rectangle2.DisplayDetails();
 
             Console.ReadKey();
         }
