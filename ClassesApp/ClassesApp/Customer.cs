04efe15b4f18d7a26c68baa3bf71f6e390b24bcc
aledifon <aledifon89@gmail.com>
2024-06-15 19:14:57 +0200

    readonly modifier example

--- ClassesApp/ClassesApp/Customer.cs
+++ ClassesApp/ClassesApp/Customer.cs
@@ -8,13 +8,21 @@ namespace ClassesApp
 {
     internal class Customer
     {
+        // Static field to hold the next ID available
+        private static int nextId = 0;
+
+        private readonly int _id;
+
         public string Name { get; set; }
         public string Address { get; set; }
         public string ContactNumber { get; set; }
 
+        public int Id {get => _id;}
+
         // Default Constructor
         public Customer()
-        {
+        {            
+            _id = nextId++;                 //Increment The next Customer Id, and assign the current one
             Name = "Default Name";
             Address = "No address";
             ContactNumber = "No contactNumber";            
@@ -22,7 +30,8 @@ namespace ClassesApp
 
         // Default/optional parameter addresss and contactNumber
         public Customer(string name, string address = "NA" , string contactNumber = "NA")
-        {
+        {            
+            _id = nextId++;                 //Increment The next Customer Id, and assign the current one
             Name = name;
             Address = address;
             ContactNumber = contactNumber;
@@ -34,6 +43,11 @@ namespace ClassesApp
             Name = name;
             Address = address;
             ContactNumber = contactNumber;
+        }
+
+        public void GetDetails()
+        {
+            Console.WriteLine($"Details about the customer: Name is {Name} and Id is {Id}");
         }
 
         public static void DoSomeCustomerStuff()
--- ClassesApp/ClassesApp/Program.cs
+++ ClassesApp/ClassesApp/Program.cs
@@ -5,14 +5,28 @@
     {
         static void Main(string[] args)
         {
-            Car car = new Car();
-            Car car2 = new Car();
-            Car car3 = new Car("A3","Audi",false);
-            car3.Drive();
+            //Car car = new Car();
+            //Car car2 = new Car();
+            //Car car3 = new Car("A3","Audi",false);
+            //car3.Drive();
 
-            // accesing the public static variable NumberOfCars of the Car Class
-            Console.WriteLine("Number of cars produced: " + Car.NumberOfCars);            
-            
+            //// accesing the public static variable NumberOfCars of the Car Class
+            //Console.WriteLine("Number of cars produced: " + Car.NumberOfCars);            
+
+            // Reading a private read-only field
+
+            Customer customer = new Customer();
+            Customer customer2 = new Customer("John Doe");
+            Customer customer3 = new Customer();
+
+            Console.WriteLine("The 1st Customer ID is " + customer.Id + "\n");
+            Console.WriteLine("The 2nd Customer ID is " + customer2.Id + "\n");
+            Console.WriteLine("The 3rd Customer ID is " + customer3.Id + "\n");
+
+            customer.GetDetails();
+            customer2.GetDetails();
+            customer3.GetDetails();
+
             Console.ReadKey();
         }
 
