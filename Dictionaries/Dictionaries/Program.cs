a125634566e05f36f20e5cb1be3e2e1b932c4571
alejandro.diazfontal <alejandro.diazfontalva@less-sa.com>
2024-07-09 17:10:43 +0200

    More samples related with Dicctionaries

--- Dictionaries/Dictionaries/Program.cs
+++ Dictionaries/Dictionaries/Program.cs
@@ -11,10 +11,25 @@
             // Adding Items to a Dictionary
             employees.Add(101, "John Doe");
             employees.Add(102, "Bob Smith");
+            employees.Add(103, "Rob Smith");
+            employees.Add(104, "Flob Smith");
+            employees.Add(105, "Dob Smith");
 
             // access items in a dictionary
             string name = employees[101];
             Console.WriteLine(name);
+
+            // Update data in a dictionary
+            employees[102] = "Jane Smith";
+
+            // remove an item from a dictionary
+            employees.Remove(101);
+
+            foreach (KeyValuePair<> kvp in employees)
+            {
+
+            }
+
 
             Console.ReadKey();
         }
