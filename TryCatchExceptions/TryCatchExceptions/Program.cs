f62ac9d039128664c21883d3ccd8a0d010dd1292
alejandro.diazfontal <alejandro.diazfontalva@less-sa.com>
2024-07-12 13:31:57 +0200

    Started TryCatchExceptions samples

---
+++ TryCatchExceptions/TryCatchExceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TryCatchExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            try
+            {
+                Console.WriteLine("Please enter a number");
+                int num2 = int.Parse(Console.ReadLine());
+                int num1 = 2;
+
+                result = num1 / num2;
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            
+
+            Console.WriteLine("Result: " + result);
+            Console.ReadKey();
+        }
+    }
+}
