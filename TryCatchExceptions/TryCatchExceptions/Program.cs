06756161e6e4c437324e615876a5ba9ab9444268
alejandro.diazfontal <alejandro.diazfontalva@less-sa.com>
2024-07-12 15:36:28 +0200

    throw Exceptions tests

--- TryCatchExceptions/TryCatchExceptions/Program.cs
+++ TryCatchExceptions/TryCatchExceptions/Program.cs
@@ -1,27 +1,81 @@
-﻿namespace TryCatchExceptions
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace TryCatchExceptions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int result = 0;
+            ///////////////////
+            ///CODE Challenge//
+            ///////////////////
+            //try
+            //{
+            //    Console.Write("Trying...");
+            //}
+            //finally
+            //{
+            //    Console.Write("Finally executed.\n");
+            //}
 
-            try
-            {
-                Console.WriteLine("Please enter a number");
-                int num2 = int.Parse(Console.ReadLine());
-                int num1 = 2;
+            //int result = 0;
 
-                result = num1 / num2;
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }
-            
+            //Debug.WriteLine("Main method is running");
 
-            Console.WriteLine("Result: " + result);
+            //try
+            //{
+            //    Console.WriteLine("Please enter a number");
+            //    int num2 = int.Parse(Console.ReadLine());
+            //    int num1 = 2;
+
+            //    result = num1 / num2;
+            //}
+            //catch (Exception ex) 
+            //{
+            //    Console.WriteLine("Error: " + ex.Message);
+            //    // This next line is only executed during "Debugging"
+            //    Debug.WriteLine(ex.ToString());
+            //}
+            //finally
+            //{
+            //    // Code to cleanup or finalize
+            //    // ideal for cleaning up resources,
+            //    // like closing file streams or database connections.
+
+            //    Console.WriteLine("This always executes");
+            //}
+
+            //Console.WriteLine("Result: " + result);
+            //Console.ReadKey();            
+
+
+
+
+            Console.WriteLine("Please enter your age");
+
+            // Non-static GetUserAge method
+            //Program program = new Program();            
+            //program.GetUserAge(Console.ReadLine());
+
+            // Static GetUserAge method
+            GetUserAge(Console.ReadLine());
+
             Console.ReadKey();
+        }
+
+        static int GetUserAge(string input)
+        {
+            int age = 0;
+            if (!int.TryParse(input, out age))
+            {
+                throw new Exception("You didn't enter a valid age");
+            }
+            if (age < 0 || age > 120)
+            {
+                throw new Exception("Your age must be between 0 and 120.");
+            }
+            return age;
         }
     }
 }
