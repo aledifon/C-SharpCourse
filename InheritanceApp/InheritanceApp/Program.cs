737d0cab67f1b6669917ba1f05e762596ef8d49c
aledifon <aledifon89@gmail.com>
2024-09-10 13:44:23 +0200

    Created InheritanceApp

---
+++ InheritanceApp/InheritanceApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace InheritanceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    // Base Class (Parent Class or Superclass): The class whose members are inherited
+    class Animal
+    {
+        public void Eat() 
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    class Dog: Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+
+
+}
+
+
+
