0559b12e7e117a5baa3ac13c6d76bf3fa6b6936e
aledifon <aledifon89@gmail.com>
2024-09-23 14:00:55 +0200

    Created AccessModifiers Project & More modifs related with Inheritance topic

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass baseclase = new BaseClass();
+            baseclase.ShowFields();
+
+            DerivedClass derivedclase = new DerivedClass();
+            derivedclase.AccessFields();
+            derivedclase.ShowFields();
+
+            Console.ReadKey();
+        }
+    }
+
+    class BaseClass
+    {
+        // access modifiers
+        public int publicField;
+        protected int protectedField;
+        private int privateField =5;        
+
+        public void ShowFields()
+        {
+            Console.WriteLine($"Public: {publicField}, " +
+                $"Protected: {protectedField}, Private: {privateField}");
+        }
+    }
+
+    class DerivedClass: BaseClass
+    {
+        public void AccessFields()
+        {
+            publicField = 1;
+            protectedField = 2;
+            //privateField = 3;
+        }        
+    }
+}
--- InheritanceApp/InheritanceApp/Program.cs
+++ InheritanceApp/InheritanceApp/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Dog myDog = new Dog();
+            //myDog.MakeSound();
+            ////myDog.Eat();
+
+            //Cat mycat = new Cat();
+            //mycat.MakeSound();
+
+            Employee joe = new Employee("Joe", 36);
+            joe.DisplayPersonInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        // Base class constructor
+        public Person(string name, int age)
+        {            
+            Name = name;
+            Age = age;
+            Console.WriteLine("Person constructor called");
+        }
+
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+        
+    }
+
+
+    public class Employee : Person
+    {
+        public Employee(string name, int age) : base(name, age)
+        {
+            Console.WriteLine("Employee (derived class) constructor called");
         }
     }
 
@@ -15,16 +54,49 @@
         {
             Console.WriteLine("Eating...");
         }
-    }
 
+        // Making this method virtual allows us to override it
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal makes a generic sound");
+        }
+    } 
+
+
+    // Single inheritance example
+    // Derived Class (Child Class or Subclass):
+    // The class that inherits the members of the base class
     class Dog: Animal
     {
-        public void Bark()
+        public override void MakeSound()
         {
+            base.MakeSound();
             Console.WriteLine("Barking...");
         }
     }
 
+    // Hierarquical inheritance
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            Console.WriteLine("Cat is meowing");
+        }
+    }
+
+    // Multilevel inheritance example
+    // A breed of dog
+    class Collie : Dog
+    {
+        public void GoingNuts()
+        {
+            Console.WriteLine("Collie going Nuts");
+        }
+    }
+
+    // Also there is a 4th type of inheritance (Multiple inheritance)
+    // only possible through interfaces
 
 }
 
