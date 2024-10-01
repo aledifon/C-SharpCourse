b1023575ee7ea5de32d21a2febd3fcaf03b0a626
aledifon <aledifon89@gmail.com>
2024-10-01 15:17:33 +0200

    Constructor Dependency Injection (DI) Example

--- DependencyInjectionApp/DependencyInjectionApp/Program.cs
+++ DependencyInjectionApp/DependencyInjectionApp/Program.cs
@@ -1,10 +1,64 @@
 ﻿namespace DependencyInjectionApp
-{
+{    
+    public class Hammer
+    {
+        public void Use()
+        {
+            Console.WriteLine("Hammering Nails!");
+        }
+    }
+
+    public class Saw
+    {
+        public void Use()
+        {
+            Console.WriteLine("Sawing wood!");
+        }
+    }
+
+    //public class Tools
+    //{
+    //    public Hammer hammer;
+    //    public Saw saw;
+    //}
+
+    public class Builder
+    {
+        private Hammer _hammer;
+        private Saw _saw;
+
+        //public Builder(Tools tools)
+        //{
+        //    _hammer = tools.hammer; // Builder is responsible for creating its dependencies
+        //    _saw = tools.saw;
+        //}
+
+        // Constructor Dependency Injection (DI)
+        public Builder(Hammer hammer, Saw saw)
+        {
+            _hammer = hammer; // Builder is responsible for creating its dependencies
+            _saw = saw;
+        }
+
+        public void BuildHouse()
+        {
+            _hammer.Use();
+            _saw.Use();
+            Console.WriteLine("House built");
+        }
+    }    
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Hammer hammer = new Hammer();
+            Saw saw = new Saw();    
+            Builder builder = new Builder(hammer, saw);
+
+            builder.BuildHouse();   
+
+            Console.ReadKey();
         }
     }
 }
