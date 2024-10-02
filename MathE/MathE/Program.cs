28b3c2e36d89d9524e48b0bbe836c36da634b1cf
aledifon <aledifon89@gmail.com>
2024-10-02 16:46:20 +0200

    Finished the Math classes examples

--- MathE/MathE/Program.cs
+++ MathE/MathE/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ceiling: " + Math.Ceiling(15.3));
+            Console.WriteLine("Floor: " + Math.Floor(15.3));
+
+            int num1 = 13;
+            int num2 = 9;
+            Console.WriteLine("Lower of num1 {0} and num2 {1} is {2}", num1, num2, Math.Min(num1,num2));
+            Console.WriteLine("Higher of num1 {0} and num2 {1} is {2}", num1, num2, Math.Max(num1, num2));
+
+            Console.WriteLine("3 to the power of 5 is {0}", Math.Pow(3,5));
+            Console.WriteLine("PI is " + Math.PI);
+
+            Console.WriteLine("The square root of 25 is " + Math.Sqrt(25));
+
+            Console.WriteLine("Always positive is "+ Math.Abs(-25));
+            Console.WriteLine("cos of 1 is: "+ Math.Cos(1));
+            
+
+            Console.ReadKey();           
         }
     }
 }
