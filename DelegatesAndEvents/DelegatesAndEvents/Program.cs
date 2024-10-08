7b57c7ca9f56c3d19e67dfac7762749f58b8b03f
aledifon <aledifon89@gmail.com>
2024-10-08 13:47:24 +0200

    Declaration, instantiation and invocation of delegates

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace DelegatesAndEvents
 {
+    // 1. Declaration:
+    // Is possible to declare delegates outside of the class, 
+    // indeed is preferable
+    public delegate void Notify(string message);
+
     internal class Program
     {
+        //// 1. Declaration:
+        //public delegate void Notify(string message);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Delegates define a method signature,
+            // and any method assigned to a delegate must match this signature.
+
+            // 1. Declaration:
+
+            // 2. Instantiation:
+            Notify notifyDelegate = ShowMessage;
+            //Notify notifyDelegate = new Notify(ShowMessage);
+
+            // 3. Invocation:
+            notifyDelegate("Hello Delegates!");
+
+
+            Console.ReadKey();
+        }
+
+        static void ShowMessage(string message2)
+        {
+            Console.WriteLine(message2);
         }
     }
 }
