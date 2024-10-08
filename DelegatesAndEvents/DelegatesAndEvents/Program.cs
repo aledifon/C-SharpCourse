1fce8dd28d899917900cf7ecb8361c2ced9f0ae8
aledifon <aledifon89@gmail.com>
2024-10-08 14:10:21 +0200

    Generic datataype example

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -1,33 +1,69 @@
 ﻿namespace DelegatesAndEvents
-{    
-    public delegate void LogHandler(string message);
+{
+    //public delegate void LogHandler(string message);
 
-    public class Logger
-    {
-        public void LogToConsole(string message)
-        {
-            Console.WriteLine("Console Log: " + message);
-        }
+    //public class Logger
+    //{
+    //    public void LogToConsole(string message)
+    //    {
+    //        Console.WriteLine("Console Log: " + message);
+    //    }
 
-        public void LogToFile(string message)
-        {
-            Console.WriteLine("File log: " + message);
-        }
-    }
+    //    public void LogToFile(string message)
+    //    {
+    //        Console.WriteLine("File log: " + message);
+    //    }
+    //}
 
     internal class Program
-    {        
+    {
         static void Main(string[] args)
-        {            
+        {
 
-            Logger logger = new Logger();
-            LogHandler logHandler = logger.LogToConsole;
-            logHandler("Logging to console");
+            //Logger logger = new Logger();
+            //LogHandler logHandler = logger.LogToConsole;
+            //logHandler("Logging to console");
 
-            logHandler = logger.LogToFile;
-            logHandler("Log some stuff");
+            //logHandler = logger.LogToFile;
+            //logHandler("Log some stuff");
 
+            int[] intArray = { 1, 2, 3, 4, 5};
+            string[] stringArray = { "One", "Two", "Three", "Four"};
+
+            // Generic method calls
+            PrintArray(intArray);
+            PrintArray(stringArray);
             Console.ReadKey();
-        }        
+        }
+
+        // GENERICS example
+
+        public static void PrintIntArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void PrintStringArray(string[] array)
+        {
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        // This method replaces the two previous methods by using generics "T"
+        // "T" replaces any datatype
+
+        // a generic method, that accepts a generic datatype
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach(T item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
     }
 }
