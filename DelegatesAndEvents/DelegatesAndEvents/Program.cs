30cd9b033acfa916e08539f56771dc72df3f8e63
aledifon <aledifon89@gmail.com>
2024-10-09 13:28:13 +0200

    Sorting algorithm with generics and Delegates

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -1,69 +1,75 @@
 ﻿namespace DelegatesAndEvents
-{
-    //public delegate void LogHandler(string message);
+{    
+    public delegate int Comparison<T>(T x, T y);
 
-    //public class Logger
-    //{
-    //    public void LogToConsole(string message)
-    //    {
-    //        Console.WriteLine("Console Log: " + message);
-    //    }
+    public class Person
+    {        
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
 
-    //    public void LogToFile(string message)
-    //    {
-    //        Console.WriteLine("File log: " + message);
-    //    }
-    //}
+    public class PersonSorter
+    {
+        public void Sort(Person[] people, Comparison<Person> comparison)
+        {
+            for (int i = 0; i < people.Length - 1; i++)
+            {
+                // Compare people[i] and people[j] using the provided comparison delegate
+                for (int j = i + 1; j < people.Length; j++)
+                {
+                    if (comparison(people[i], people[j]) > 0)
+                    {
+                        // Swap people[i] and people[j] if they are in the wrong order
+                        Person temp = people[i];
+                        people[i] = people[j];  
+                        people[j] = temp;
+
+                        //people[0] = Alice;
+                        //people[1] = Bob;
+                        //people[2] = Charlie;
+                    }
+                }
+            }
+        }
+    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person[] people =
+            {
+                new Person { Name = "Alice", Age = 30},
+                new Person { Name = "Bob", Age = 25},
+                new Person { Name = "Denis", Age = 36},
+                new Person { Name = "Charlie", Age = 35},
+            };           
+            
+            PersonSorter personSorter = new PersonSorter();
+            personSorter.Sort(people, CompareByAge);
 
-            //Logger logger = new Logger();
-            //LogHandler logHandler = logger.LogToConsole;
-            //logHandler("Logging to console");
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"Name: {person.Name}. Age: {person.Age}");
+            }
 
-            //logHandler = logger.LogToFile;
-            //logHandler("Log some stuff");
+            personSorter.Sort(people, CompareByName);
 
-            int[] intArray = { 1, 2, 3, 4, 5};
-            string[] stringArray = { "One", "Two", "Three", "Four"};
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"Name: {person.Name}. Age: {person.Age}");
+            }
 
-            // Generic method calls
-            PrintArray(intArray);
-            PrintArray(stringArray);
             Console.ReadKey();
-        }
+        } 
 
-        // GENERICS example
-
-        public static void PrintIntArray(int[] array)
+        static int CompareByAge(Person x, Person y)
         {
-            foreach (int item in array)
-            {
-                Console.WriteLine(item);
-            }
+            return x.Age.CompareTo(y.Age);
         }
-        public static void PrintStringArray(string[] array)
+        static int CompareByName(Person x, Person y)
         {
-            foreach (string item in array)
-            {
-                Console.WriteLine(item);
-            }
+            return x.Name.CompareTo(y.Name);
         }
-
-        // This method replaces the two previous methods by using generics "T"
-        // "T" replaces any datatype
-
-        // a generic method, that accepts a generic datatype
-        public static void PrintArray<T>(T[] array)
-        {
-            foreach(T item in array)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
     }
 }
