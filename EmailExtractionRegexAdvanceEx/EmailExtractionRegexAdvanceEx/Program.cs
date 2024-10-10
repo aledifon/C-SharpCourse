c98cc2ce40bf2f514a1cd672a5e0f1194967dbeb
aledifon <aledifon89@gmail.com>
2024-10-11 01:10:55 +0200

    Finished Regular Expressions with Regex examples & Advanced Exercise

---
+++ EmailExtractionRegexAdvanceEx/EmailExtractionRegexAdvanceEx/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EmailExtractionRegexAdvanceEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Contact us at support@example.com or sales@example.org";
+
+            ExtractPatterns(input);
+            Console.ReadKey();
+        }
+
+        static void ExtractPatterns(string input)
+        {
+            string pattern = @"(\w+)@(\w+)(\.\w+)";
+            Regex regex = new Regex(pattern);            
+
+            MatchCollection matchCollection = regex.Matches(input);
+
+            //Console.WriteLine(matchCollection[0].Value);  // In case of only 1 match            
+
+            foreach (Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine(group[0].Value);
+            }
+        }
+    }
+}
