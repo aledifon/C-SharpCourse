a7c5934a9e9ebf713c7f58148e3c3dfbb8b1bb8b
aledifon <aledifon89@gmail.com>
2024-10-10 15:14:57 +0200

    Event publisher & subscribers examples mixed with properties

--- EventsApp/EventsApp/Program.cs
+++ EventsApp/EventsApp/Program.cs
@@ -1,38 +1,103 @@
 ﻿namespace EventsApp
 {
-    public delegate void Notify(string message);
+    public delegate void TemperatureChangeHandler(string message);    
 
-    public class EventPublisher 
+    // Event publisher
+    public class TemperatureMonitor
     {
-        // The "On" prefix makes it immediately clear that the method
-        // is associated with an event.
-        // It signifies that the method is not just a regular method but
-        // one that is called when a specific event occurs.
-        public event Notify OnNotify;
+        public event TemperatureChangeHandler OnRaiseTemperatureChanged;
+        public event TemperatureChangeHandler OnDropTemperatureChanged;
 
-        public void RaiseEvent(string message)
+        private int _temperature;
+        public int Temperature 
+        {   get { return _temperature; }
+            set
+            {
+                _temperature = value;
+                if (_temperature > 30)
+                {
+                    //_temperature= (int)(_temperature * 1.8) + 32;
+
+                    // RAISE EVENT!!                    
+                    RaiseTemperatureChangedEvent("Temperature is above threshold!");
+                }
+                else if (_temperature < 10) 
+                {
+                    // DROP EVENT!!                    
+                    DropTemperatureChangedEvent("Temperature is below threshold!");
+                }
+            } 
+        }
+
+        //// Class constructor
+        //public TemperatureMonitor(int temperature)
+        //{
+        //    Temperature=temperature;
+        //}
+
+        protected virtual void RaiseTemperatureChangedEvent(string message)
         {
-            OnNotify?.Invoke(message);  // Invoke the event if there are any subscribers
+            OnRaiseTemperatureChanged?.Invoke(message);
+        }
+        protected virtual void DropTemperatureChangedEvent(string message)
+        {
+            OnDropTemperatureChanged?.Invoke(message);
+        }
+
+    }
+
+    // Event subscriber
+    public class TemperatureAlert
+    {
+        public void OnTemperatureChanged(string message)
+        {
+            Console.WriteLine("Temperature System Handler\nTemperature Alert!: " + message);
         }
     }
 
-    public class EventSubscriber
+    public class CoolingSystemAlert
     {
-        public void OnEventRaised(string message) 
+        public void OnRaiseTemperatureChanged(string message)
         {
-            Console.WriteLine("Event received: " + message);
+            Console.WriteLine("Cooling System Handler\nTemperature Alert!: " + 
+                                message + "\n" + "Enabling Cooling system");
         }
+
+        public void OnDropTemperatureChanged(string message)
+        {
+            Console.WriteLine("Cooling System Handler\nTemperature Alert!: " +
+                                message + "\n" + "Disabling Cooling system");
+        }
+    }
+
+    public class PhoneMessagesHandler
+    {
+        public void OnTemperatureChanged(string message)
+        {
+            Console.WriteLine("Phone System Handler\nTemperature alert!: " + message);
+        }        
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            EventPublisher publisher = new EventPublisher();
-            EventSubscriber subscriber = new EventSubscriber();            
-            publisher.OnNotify += subscriber.OnEventRaised;
+            TemperatureMonitor monitor = new TemperatureMonitor();
+            TemperatureAlert alert = new TemperatureAlert();
+            CoolingSystemAlert coolingSystem = new CoolingSystemAlert();
+            PhoneMessagesHandler phoneMessages = new PhoneMessagesHandler();
 
-            publisher.RaiseEvent("test");
+            monitor.OnRaiseTemperatureChanged += alert.OnTemperatureChanged;
+            monitor.OnRaiseTemperatureChanged += coolingSystem.OnRaiseTemperatureChanged;
+            monitor.OnRaiseTemperatureChanged += phoneMessages.OnTemperatureChanged;
+
+            monitor.OnDropTemperatureChanged += alert.OnTemperatureChanged;
+            monitor.OnDropTemperatureChanged += coolingSystem.OnDropTemperatureChanged;
+            monitor.OnDropTemperatureChanged += phoneMessages.OnTemperatureChanged;
+
+            monitor.Temperature = 20;
+            Console.WriteLine("Please enter the temperature");
+            monitor.Temperature = int.Parse(Console.ReadLine());
 
             Console.ReadKey();
         } 
